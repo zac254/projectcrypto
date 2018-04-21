7da378406565f1e33a994ba382659d73a45752e4
zac254 <zachsteven.znn@gmail.com>
2018-04-21 16:14:34 +0300

    Update program.cs

--- program.cs
+++ program.cs
@@ -1,8 +1,6 @@
 // Decompiled with JetBrains decompiler
-// Type: CryptoShuffler.Program
-// Assembly: CryptoShuffler, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 5F09791C-5808-4C4E-85A5-E8B78F7E37DE
-// Assembly location: C:\Users\gorno\Desktop\vm\de4dot-moded\cryptoshuffler-cleaned-cleaned-cleaned-cleaned.exe
+// Type: Programcrpto.Program
+// Assembly: Programcrypto
 
 using ClipboardHelper;
 using Microsoft.Win32;
