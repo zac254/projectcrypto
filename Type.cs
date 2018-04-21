01ae4eceef017eb59fa9b5bb5b86988277e232ed
zac254 <zachsteven.znn@gmail.com>
2018-04-21 15:41:29 +0300

    Create Type.cs

---
+++ Type.cs
@@ -0,0 +1,18 @@
+// Decompiled with JetBrains decompiler
+// Type: Projectcrypto.Type
+
+namespace Projectcrpto
+{
+  internal enum Type
+  {
+    BTC,
+    LTC,
+    ETH,
+    XMR,
+    Qiwi,
+    WMR,
+    WMZ,
+    WME,
+    Steam,
+  }
+}
